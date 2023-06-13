b2c301face6a93b67fe3661841572f8f19543171
JakariaJishan <jakariajishan987@gmail.com>
2023-06-14 02:11:27 +0600

    Configure linters

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,11 +1,8 @@
-
 class Api::V1::MessagesController < ApplicationController
-    def index
-    end
-    
-    def message
-      @message = Message.order('RANDOM()').first
-      render json: @message
-    end
+  def index; end
+
+  def message
+    @message = Message.order('RANDOM()').first
+    render json: @message
   end
-  
+end
--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get message" do
+  test 'should get message' do
     get messages_message_url
     assert_response :success
   end
